a5af41eafaa3068986ebd52ccfaacdc90b6cd8f2
FunStudios <133611101+FunStudios@users.noreply.github.com>
2024-09-09 18:47:56 -0500

    Create backend_code.py

---
+++ backend_code.py
@@ -0,0 +1,5 @@
+pip install -U scratchattach
+import scratchattach as scratch3
+session = scratch3.login("a-free-follower", "001542016")
+while 1==1:
+user.follow(user.follower_names(limit=1, offset=0))
