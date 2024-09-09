76e00f64d66ec801944d51aca410b8323a386c81
FunStudios <133611101+FunStudios@users.noreply.github.com>
2024-09-09 18:51:35 -0500

    Update backend_code.py

--- backend_code.py
+++ backend_code.py
@@ -1,5 +1,7 @@
 pip install -U scratchattach
 import scratchattach as scratch3
+import time
 session = scratch3.login("a-free-follower", "001542016")
 while 1==1:
-user.follow(user.follower_names(limit=1, offset=0))
+  user.follow(user.follower_names(limit=1, offset=0))
+  time.sleep(3) # Sleep for 3 seconds
